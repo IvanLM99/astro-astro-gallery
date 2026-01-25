82fe64cfc9894e91ffe2653ea96c849bdb8af543
Iván López Muñoz <150972967+IvanLM99@users.noreply.github.com>
2026-01-26 00:51:43 +0100

    Update base path in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://ivanlm99.github.io', // Your GitHub URL
-  base: '/my-cosmos', // If your repo name is just 'ivanlm99.github.io' keep this '/'. If it is 'portfolio', change to '/portfolio'
+  base: '/astro-astro-gallery', // If your repo name is just 'ivanlm99.github.io' keep this '/'. If it is 'portfolio', change to '/portfolio'
   build: {
     assets: '_astro'
   }
