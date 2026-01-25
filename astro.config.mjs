0cfc517f8e064e2ffcc623aaa005ea2bb1a73073
ivanlm99 <ivan1999spain@gmail.com>
2026-01-26 00:18:07 +0100

    Initial launch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://ivanlm99.github.io', // Your GitHub URL
+  base: '/my-cosmos', // If your repo name is just 'ivanlm99.github.io' keep this '/'. If it is 'portfolio', change to '/portfolio'
+  build: {
+    assets: '_astro'
+  }
+});
