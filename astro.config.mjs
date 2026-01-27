1d0677ce908e77fd2f8bed2dd65f17ad2a6f8ee0
Iván López Muñoz <150972967+IvanLM99@users.noreply.github.com>
2026-01-27 01:40:17 +0100

    Change asset directory from '_astro' to 'assets'

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ export default defineConfig({
   site: 'https://ivanlm99.github.io', // Your GitHub URL
   base: '/astro-astro-gallery', // If your repo name is just 'ivanlm99.github.io' keep this '/'. If it is 'portfolio', change to '/portfolio'
   build: {
-    assets: '_astro'
+    assets: 'assets'
   }
 });
