4712968719d982fc137c2aa0eb5e0028315807b2
ilopezmu <ilopezmu@riu.com>
2026-07-06 16:32:48 +0200

    Initial commit, minus large zip file

---
+++ astro.config.mjs
@@ -0,0 +1,22 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://ivanlm99.github.io',
+  base: '/astro-gallery', // Change if your repo is named differently
+  i18n: {
+    defaultLocale: 'es',
+    locales: ['es', 'en'],
+    routing: {
+      prefixDefaultLocale: false // Makes /gallery default to Spanish, /en/gallery for English
+    }
+  },
+  build: {
+    assets: 'assets'
+  },
+  vite: {
+    build: {
+      cssCodeSplit: true, // Better performance
+      assetsInlineLimit: 4096 // Inline small assets
+    }
+  }
+});
